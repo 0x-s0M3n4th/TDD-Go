21e183d0c4773e333d6a09d57661bad759f2102e
0x-s0M3n4th <sebaitsom6297@gmail.com>
2026-08-22 10:23:20 +0530

    refactor: Add a case for empty string as argument to Hello()

--- hello.go
+++ hello.go
@@ -5,6 +5,9 @@ import "fmt" // Importing a package that contains the Println funtion
 const englishHelloPrefix = "Hello, " // Declaring a constant value
 
 func Hello(name string) string { // string -> means the func returns string value
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPrefix + name + "!"
 }
 
