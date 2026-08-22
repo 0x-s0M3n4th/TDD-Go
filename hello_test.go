827051a411e41634ff5e01de2f065d01daac49f5
0x-s0M3n4th <sebaitsom6297@gmail.com>
2026-08-22 11:27:35 +0530

    refactor: Add helper function to assert the code output

--- hello_test.go
+++ hello_test.go
@@ -7,18 +7,20 @@ func TestHello(t *testing.T) {
 	t.Run("Saying hello to people", func(t *testing.T) {
 		got := Hello("Somenath")
 		want := "Hello, Somenath!"
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	// Case when an empty string  is provided as argument
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World!"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // This tells the testing framework that  if any error occurs don't point it towards me, i am just a helper function.
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
